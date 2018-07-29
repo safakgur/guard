a4f72f476be0bc7d7afea1903b6f4f42da33bea0
Şafak Gür <s@gur.works>
2018-07-29 16:17:22 +0300

    Make type guards support inheritance chains.

--- src/Guard.Types.cs
+++ src/Guard.Types.cs
@@ -1,6 +1,9 @@
 ﻿namespace Dawn
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Threading;
 
     /// <content>Provides type preconditions.</content>
     public static partial class Guard
@@ -25,7 +28,7 @@
         public static ArgumentInfo<T> Type<T>(
             in this ArgumentInfo<object> argument, Func<object, string> message = null)
         {
-            if (argument.HasValue() && argument.Value.GetType() != typeof(T))
+            if (!TypeInfo<T>.CanBeInitializedFrom(argument.Value))
             {
                 var m = message?.Invoke(argument.Value) ?? Messages.Type(argument, typeof(T));
                 throw new ArgumentException(m, argument.Name);
@@ -54,7 +57,7 @@
         public static ArgumentInfo<object> NotType<T>(
             in this ArgumentInfo<object> argument, Func<T, string> message = null)
         {
-            if (argument.HasValue() && argument.Value.GetType() == typeof(T))
+            if (argument.HasValue() && TypeInfo<T>.CanBeInitializedFrom(argument.Value))
             {
                 var m = message?.Invoke((T)argument.Value) ?? Messages.NotType(argument, typeof(T));
                 throw new ArgumentException(m, argument.Name);
@@ -83,7 +86,7 @@
         public static ArgumentInfo<object> Type(
             in this ArgumentInfo<object> argument, Type type, Func<object, Type, string> message = null)
         {
-            if (argument.HasValue() && argument.Value.GetType() != type && type != null)
+            if (argument.HasValue() && !TypeInfo.CanBeConvertedTo(argument.Value, type))
             {
                 var m = message?.Invoke(argument.Value, type) ?? Messages.Type(argument, type);
                 throw new ArgumentException(m, argument.Name);
@@ -112,13 +115,113 @@
         public static ArgumentInfo<object> NotType(
             in this ArgumentInfo<object> argument, Type type, Func<object, Type, string> message = null)
         {
-            if (argument.HasValue() && argument.Value.GetType() == type)
+            if (argument.HasValue() && TypeInfo.CanBeConvertedTo(argument.Value, type))
             {
                 var m = message?.Invoke(argument.Value, type) ?? Messages.NotType(argument, type);
                 throw new ArgumentException(m, argument.Name);
             }
 
             return argument;
+        }
+
+        /// <summary>Provides cached utilities for <typeparamref name="T" />.</summary>
+        /// <typeparam name="T">The type.</typeparam>
+        private static class TypeInfo<T>
+        {
+            /// <summary>
+            ///     A function that determines whether the specified object
+            ///     can be converted to type <typeparamref name="T" />.
+            /// </summary>
+            public static readonly Func<object, bool> CanBeInitializedFrom = InitCanBeInitializedFrom();
+
+            /// <summary>Initializes <see cref="Contains" />.</summary>
+            /// <returns>
+            ///     A function that determines whether the specified object
+            ///     can be converted to type <typeparamref name="T" />.
+            /// </returns>
+            private static Func<object, bool> InitCanBeInitializedFrom()
+            {
+                var targetType = typeof(T);
+                var isValueType = IsValueType(targetType);
+                var isNullable = !isValueType || targetType.IsGenericType(typeof(Nullable<>));
+
+                var type = targetType;
+                var resultChain = new HashSet<Type>();
+                do
+                {
+                    resultChain.Add(type);
+                    type = type.GetBaseType();
+                }
+                while (type != null);
+                if (isValueType && isNullable)
+                    resultChain.Add(targetType.GetGenericArguments()[0]);
+
+                resultChain.TrimExcess();
+                return obj => obj is null ? isNullable : resultChain.Contains(obj.GetType());
+            }
+        }
+
+        /// <summary>Provides non-generic, cached utilities for specified types.</summary>
+        private static class TypeInfo
+        {
+            /// <summary>
+            ///     The locker that synchronizes access to
+            ///     <see cref="canBeConvertedTo" />.
+            /// </summary>
+            private static readonly ReaderWriterLockSlim locker
+                = new ReaderWriterLockSlim();
+
+            /// <summary>
+            ///     The functions that determine whether a specified object can
+            ///     be converted to the type that the function is mapped to.
+            /// </summary>
+            private static readonly Dictionary<Type, Func<object, bool>> canBeConvertedTo
+                = new Dictionary<Type, Func<object, bool>>();
+
+            /// <summary>
+            ///     Determines whether an object can be converted
+            ///     to an instance of the specified type.
+            /// </summary>
+            /// <param name="obj">The object to check.</param>
+            /// <param name="targetType">The type to check.</param>
+            /// <returns>
+            ///     <c>true</c>, if <paramref name="obj" /> can be converted
+            ///     to an instance of <paramref name="targetType" />.
+            /// </returns>
+            /// <remarks>
+            ///     Calls <see cref="TypeInfo{T}.CanBeInitializedFrom" />.
+            /// </remarks>
+            public static bool CanBeConvertedTo(object obj, Type targetType)
+            {
+                locker.EnterUpgradeableReadLock();
+                try
+                {
+                    if (!canBeConvertedTo.TryGetValue(targetType, out var func))
+                    {
+                        locker.EnterWriteLock();
+                        try
+                        {
+                            var t = typeof(TypeInfo<>).MakeGenericType(targetType);
+                            var f = Expression.Field(null, t.GetField(nameof(TypeInfo<object>.CanBeInitializedFrom)));
+                            var o = Expression.Parameter(typeof(object), "obj");
+                            var c = Expression.Invoke(f, o);
+                            var l = Expression.Lambda<Func<object, bool>>(c, o);
+                            func = l.Compile();
+                            canBeConvertedTo[targetType] = func;
+                        }
+                        finally
+                        {
+                            locker.ExitWriteLock();
+                        }
+                    }
+
+                    return func(obj);
+                }
+                finally
+                {
+                    locker.ExitUpgradeableReadLock();
+                }
+            }
         }
     }
 }
