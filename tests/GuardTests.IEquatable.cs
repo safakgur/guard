d9a2e55bbe9b76991bbc1e077201e4303c329f33
Şafak Gür <s+github@gur.uk>
2018-07-29 12:42:32 +0300

    Add equatable tests.

--- tests/GuardTests.IEquatable.cs
+++ tests/GuardTests.IEquatable.cs
@@ -7,6 +7,83 @@
     {
         [Fact(DisplayName = "Guard supports equality preconditions.")]
         public void GuardSupportsEquatables()
-            => throw new NotImplementedException();
+        {
+            var message = RandomMessage;
+
+            // Default.
+            var zero = 0;
+            var zeroArg = Guard.Argument(() => zero);
+            zeroArg.Default();
+
+            var one = 1;
+            Assert.Throws<ArgumentException>(
+                nameof(one), () => Guard.Argument(() => one).Default());
+
+            var ex = Assert.Throws<ArgumentException>(
+                nameof(one), () => Guard.Argument(() => one).Default(i =>
+                {
+                    Assert.Equal(one, i);
+                    return message;
+                }));
+
+            Assert.StartsWith(message, ex.Message);
+
+            // Not default.
+            var oneArg = Guard.Argument(() => one);
+            oneArg.NotDefault();
+
+            Assert.Throws<ArgumentException>(
+                nameof(zero), () => Guard.Argument(() => zero).NotDefault());
+
+            ex = Assert.Throws<ArgumentException>(
+                nameof(zero), () => Guard.Argument(() => zero).NotDefault(i =>
+                {
+                    Assert.Equal(zero, i);
+                    return message;
+                }));
+
+            Assert.StartsWith(message, ex.Message);
+
+            // Class equals.
+            var nullRef = null as string;
+            var nullRefArg = Guard.Argument(() => nullRef);
+
+            var a = "A";
+            var aArg = Guard.Argument(() => a);
+
+            var a2 = 'A'.ToString();
+            nullRefArg.Equal(a2);
+            aArg.Equal(a2);
+
+            var b = "B";
+            Assert.Throws<ArgumentException>(
+                nameof(a), () => Guard.Argument(() => a).Equal(b));
+
+            ex = Assert.Throws<ArgumentException>(
+                nameof(a), () => Guard.Argument(() => a).Equal(b, (s1, s2) =>
+                {
+                    Assert.Equal(a, s1);
+                    Assert.Equal(b, s2);
+                    return message;
+                }));
+
+            Assert.StartsWith(message, ex.Message);
+
+            // Class not equals.
+            nullRefArg.NotEqual(b);
+            aArg.NotEqual(b);
+
+            Assert.Throws<ArgumentException>(
+                nameof(a), () => Guard.Argument(() => a).NotEqual(a2));
+
+            ex = Assert.Throws<ArgumentException>(
+                nameof(a), () => Guard.Argument(() => a).NotEqual(a2, s =>
+                {
+                    Assert.Equal(a, s);
+                    return message;
+                }));
+
+            Assert.StartsWith(message, ex.Message);
+        }
     }
 }
