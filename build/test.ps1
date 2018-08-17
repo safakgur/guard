326fe88121bb28085261723f09ee4b0574441be1
Şafak Gür <s+github@gur.uk>
2018-08-18 02:59:55 +0300

    Run CI builds on Ubuntu.

---
+++ build/test.ps1
@@ -0,0 +1,25 @@
+param(
+    [switch]$NoInstall=$false,
+    [string]$Configuration="Release",
+    [switch]$Coverage=$false,
+    [string]$CoverageFormat="opencover",
+    [string]$CoverageOutput="../artifacts/coverage.xml"
+)
+
+IF ($NoInstall -eq $false)
+{
+    & $PSScriptRoot/install.ps1
+}
+
+$SdkPath = "../artifacts/dotnet/dotnet.exe"
+IF (-not (Test-Path $SdkPath))
+{
+    $SdkPath = "dotnet"
+}
+
+& $SdkPath restore $PSScriptRoot/../src --verbosity m
+& $SdkPath test $PSScriptRoot/../tests/ `
+    /p:DebugType=full `
+    /p:CollectCoverage=$Coverage `
+    /p:CoverletOutputFormat=$CoverageFormat `
+    /p:CoverletOutput="$CoverageOutput"
