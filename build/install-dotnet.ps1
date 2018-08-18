f0e2efe6ca5077f79037cdc720032e1fb1eb644a
Şafak Gür <s+github@gur.uk>
2018-08-18 16:08:31 +0300

    install-dotnet.ps1 gets both 2.1.4 and 1.1.9 SDKs

--- build/install-dotnet.ps1
+++ build/install-dotnet.ps1
@@ -1,20 +1,25 @@
-$Version = "2.1.302";
 $Dir = "$PSScriptRoot/../artifacts/dotnet";
 
-IF ($IsWindows -or $env:OS -like "Windows*")
-{
-    $Get = "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; " +
-        "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1')))"
+function Install-Sdk {
+    param([string]$Version)
+    IF ($IsWindows -or $env:OS -like "Windows*")
+    {
+        $Get = "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; " +
+            "&([scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1')))"
 
-    $Shell = "powershell"
-    if (Get-Command "pwsh" -ErrorAction SilentlyContinue) 
-    { 
-       $Shell = "pwsh"
+        $Shell = "powershell"
+        if (Get-Command "pwsh" -ErrorAction SilentlyContinue) 
+        { 
+           $Shell = "pwsh"
+        }
+
+        & $Shell -NoProfile -ExecutionPolicy unrestricted -Command $Get -Channel Current -Version $Version -InstallDir $Dir -NoPath
     }
+    ELSE
+    {
+        curl -s https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --version $Version --install-dir $Dir --no-path
+    }
+}
 
-    & $Shell -NoProfile -ExecutionPolicy unrestricted -Command $Get -Channel Current -Version $Version -InstallDir $Dir -NoPath
-}
-ELSE
-{
-    curl -s https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current --version $Version --install-dir $Dir --no-path
-}
+Install-Sdk -Version "2.1.400"
+Install-Sdk -Version "1.1.9"
