82560032cc5598d5a5a01a06230496c5e867362d
Şafak Gür <s@gur.works>
2018-08-30 16:35:22 +0300

    AppVeyor script exit with the correct code

--- build/appveyor.ps1
+++ build/appveyor.ps1
@@ -1,14 +1,19 @@
+$TestResult
 IF ($IsWindows -or $env:OS -like "Windows*")
 {
     & $PSScriptRoot/test.ps1 -NoInstall -Configuration CI -Coverage
+    $TestResult = $lastexitcode
 
-	choco install codecov --no-progress
+    choco install codecov --no-progress
     IF ($lastexitcode -eq 0)
     {
-	    codecov -f $PSScriptRoot/../artifacts/coverage.xml
+        codecov -f $PSScriptRoot/../artifacts/coverage.xml
     }
 }
 ELSE
 {
     & $PSScriptRoot/test.ps1 -NoInstall -Configuration Release
+    $TestResult = $lastexitcode
 }
+
+exit $TestResult
