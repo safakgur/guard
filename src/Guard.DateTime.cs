757e730515643882413bb0f61db9cc1082d7e9ce
Şafak Gür <s@gur.works>
2020-03-01 16:02:10 +0000

    Add KindSpecified/Unspecified guards for DateTime

---
+++ src/Guard.DateTime.cs
@@ -0,0 +1,134 @@
+﻿#nullable enable
+
+using System;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace Dawn
+{
+    /// <content>Provides preconditions for <see cref="DateTime" /> arguments.</content>
+    public static partial class Guard
+    {
+        /// <summary>
+        ///     Requires the date-time argument to have its <see cref="DateTime.Kind" /> specified,
+        ///     i.e. not to have it as <see cref="DateTimeKind.Unspecified" />.
+        /// </summary>
+        /// <param name="argument">The argument.</param>
+        /// <param name="message">
+        ///     The message of the exception that will be thrown if the precondition is not satisfied.
+        /// </param>
+        /// <returns><paramref name="argument" />.</returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="argument" />'s <see cref="DateTime.Kind" /> property is
+        ///     <see cref="DateTimeKind.Unspecified" />.
+        /// </exception>
+        [AssertionMethod]
+        [DebuggerStepThrough]
+        [GuardFunction("DateTime", "gtks")]
+        public static ref readonly ArgumentInfo<DateTime> KindSpecified(
+            in this ArgumentInfo<DateTime> argument, Func<DateTime, string>? message = null)
+        {
+            if (argument.Value.Kind == DateTimeKind.Unspecified)
+            {
+                var m = message?.Invoke(argument.Value) ?? Messages.KindSpecified(argument);
+                throw Fail(new ArgumentException(m, argument.Name));
+            }
+
+            return ref argument;
+        }
+
+        /// <summary>
+        ///     Requires the date-time argument to either be <c>null</c> or have its
+        ///     <see cref="DateTime.Kind" /> specified, i.e. not to have it as
+        ///     <see cref="DateTimeKind.Unspecified" />.
+        /// </summary>
+        /// <param name="argument">The argument.</param>
+        /// <param name="message">
+        ///     The message of the exception that will be thrown if the precondition is not satisfied.
+        /// </param>
+        /// <returns><paramref name="argument" />.</returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="argument" />'s <see cref="DateTime.Kind" /> property is
+        ///     <see cref="DateTimeKind.Unspecified" />.
+        /// </exception>
+        [AssertionMethod]
+        [DebuggerStepThrough]
+        [GuardFunction("DateTime", "gtks")]
+        public static ref readonly ArgumentInfo<DateTime?> KindSpecified(
+            in this ArgumentInfo<DateTime?> argument, Func<DateTime, string>? message = null)
+        {
+            if (argument.HasValue())
+            {
+                var value = argument.GetValueOrDefault();
+                if (value.Kind == DateTimeKind.Unspecified)
+                {
+                    var m = message?.Invoke(value) ?? Messages.KindSpecified(argument);
+                    throw Fail(new ArgumentException(m, argument.Name));
+                }
+            }
+
+            return ref argument;
+        }
+
+        /// <summary>
+        ///     Requires the date-time argument not to have its <see cref="DateTime.Kind" />
+        ///     specified, i.e. to have it as <see cref="DateTimeKind.Unspecified" />.
+        /// </summary>
+        /// <param name="argument">The argument.</param>
+        /// <param name="message">
+        ///     The message of the exception that will be thrown if the precondition is not satisfied.
+        /// </param>
+        /// <returns><paramref name="argument" />.</returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="argument" />'s <see cref="DateTime.Kind" /> property is not
+        ///     <see cref="DateTimeKind.Unspecified" />.
+        /// </exception>
+        [AssertionMethod]
+        [DebuggerStepThrough]
+        [GuardFunction("DateTime", "gtku")]
+        public static ref readonly ArgumentInfo<DateTime> KindUnspecified(
+            in this ArgumentInfo<DateTime> argument, Func<DateTime, string>? message = null)
+        {
+            if (argument.Value.Kind != DateTimeKind.Unspecified)
+            {
+                var m = message?.Invoke(argument.Value) ?? Messages.KindUnspecified(argument);
+                throw Fail(new ArgumentException(m, argument.Name));
+            }
+
+            return ref argument;
+        }
+
+        /// <summary>
+        ///     Requires the date-time argument either to be <c>null</c> or not to have its
+        ///     <see cref="DateTime.Kind" /> specified, i.e. to have it as
+        ///     <see cref="DateTimeKind.Unspecified" />.
+        /// </summary>
+        /// <param name="argument">The argument.</param>
+        /// <param name="message">
+        ///     The message of the exception that will be thrown if the precondition is not satisfied.
+        /// </param>
+        /// <returns><paramref name="argument" />.</returns>
+        /// <exception cref="ArgumentException">
+        ///     <paramref name="argument" />'s <see cref="DateTime.Kind" /> property is not
+        ///     <see cref="DateTimeKind.Unspecified" />.
+        /// </exception>
+        [AssertionMethod]
+        [DebuggerStepThrough]
+        [GuardFunction("DateTime", "gtku")]
+        public static ref readonly ArgumentInfo<DateTime?> KindUnspecified(
+            in this ArgumentInfo<DateTime?> argument, Func<DateTime, string>? message = null)
+        {
+            if (argument.HasValue())
+            {
+                var value = argument.GetValueOrDefault();
+                if (value.Kind != DateTimeKind.Unspecified)
+                {
+                    var m = message?.Invoke(value) ?? Messages.KindUnspecified(argument);
+                    throw Fail(new ArgumentException(m, argument.Name));
+                }
+            }
+
+            return ref argument;
+        }
+    }
+}
---
+++ tests/DateTimeTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Xunit;
+
+namespace Dawn.Tests
+{
+    public sealed class DateTimeTests : BaseTests
+    {
+        [Theory(DisplayName = "DateTime: KindSpecified/KindUnspecified")]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void KindSpecified(bool testNull)
+        {
+            var specified = testNull ? default(DateTime?) : DateTime.UtcNow;
+            var nullableSpecifiedArg = Guard.Argument(() => specified).KindSpecified();
+
+            var unspecified = testNull ? default(DateTime?) : default(DateTime);
+            var nullableUnspecifiedArg = Guard.Argument(() => unspecified).KindUnspecified();
+
+            if (testNull)
+            {
+                nullableSpecifiedArg.KindUnspecified();
+                nullableUnspecifiedArg.KindSpecified();
+                return;
+            }
+
+            ThrowsArgumentException(
+                nullableSpecifiedArg,
+                arg => arg.KindUnspecified(),
+                (arg, message) => arg.KindUnspecified(d =>
+                {
+                    Assert.Equal(specified, d);
+                    return message;
+                }));
+
+            ThrowsArgumentException(
+                nullableUnspecifiedArg,
+                arg => arg.KindSpecified(),
+                (arg, message) => arg.KindSpecified(d =>
+                {
+                    Assert.Equal(unspecified, d);
+                    return message;
+                }));
+
+            var specifiedArg = Guard.Argument(specified.Value, nameof(specified)).KindSpecified();
+            var unspecifiedArg = Guard.Argument(unspecified.Value, nameof(unspecified)).KindUnspecified();
+            ThrowsArgumentException(
+                specifiedArg,
+                arg => arg.KindUnspecified(),
+                (arg, message) => arg.KindUnspecified(d =>
+                {
+                    Assert.Equal(specified, d);
+                    return message;
+                }));
+
+            ThrowsArgumentException(
+                unspecifiedArg,
+                arg => arg.KindSpecified(),
+                (arg, message) => arg.KindSpecified(d =>
+                {
+                    Assert.Equal(unspecified, d);
+                    return message;
+                }));
+        }
+    }
+}
